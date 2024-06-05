7b2665055b0d1649e23bf6777c82f93072b1535f
blueofficer <jdhbatman@gmail.com>
2024-06-04 20:51:40 -0500

    ok main things done is bank

---
+++ banking_app.py
@@ -0,0 +1,83 @@
+import os
+
+print ("welcome to bad banking.")
+
+clear = lambda: print("\033[2J\033[H", end="", flush=True)# CLEAR FUNCTION
+
+balence = 100
+wallet = 50
+loggedin = False
+correctuser = "admin"
+correctpass = "1234"
+in_bank = True
+in_world = True
+
+save_path = "C:\\Users\\jdhba\\Desktop\\Phython\\save.txt"
+
+def save(): #save system
+    if os.path.exists(save_path) :
+        None
+
+while (True == True) : # for game loop
+    if (in_world == True and in_bank == True) : #catch in both are true
+        in_bank = False
+    if (in_world == False and in_bank == False) : #catch if both are false
+        in_world = True
+    while (in_bank == True): # banking system
+        while (loggedin != True):
+            username = input("username:")
+            password = (input("password:"))
+            
+            if (username == correctuser and password == correctpass):
+                loggedin = True
+            else :(print ("username or password is wrong"))
+        clear()
+        print ("welcome to bad banking.")
+        print ("what would you like todo?")
+        print ("1: check acount balance")
+        print ("2: deposet money")
+        print ("3: withdraw money")
+        print ("4: change username")
+        print ("5: change password")
+        print ("6: log out")
+        answer = int(input ("opption :"))
+        if (answer == 1) :
+            
+            input((balence))
+        elif (answer == 2):
+            deposet = input("how much would you like to add? : ")
+            if (deposet.isdigit):
+                deposet = int(deposet)
+                balence = balence + deposet
+                input (f"your new balense is {balence} ")
+            else :(print ("sorry only numbers allowed"))
+        elif (answer == 3):
+            withdrael = input("how much would you like to remove? : ")
+            if (withdrael.isdigit):
+                withdrael = int(withdrael)
+                if (withdrael > balence):
+                    input("sorry not enougf money in the acouint")
+                else :
+                    balence = balence - withdrael
+                    input (f"your new balense is {balence} ")
+            else :(print ("sorry only numbers allowed"))
+        elif (answer == 4):
+            newusername = input("Whats the new username? :")
+            correctuser = newusername
+            input(f"user name actped new username is : {newusername}"  )
+        elif (answer == 5):
+            print ("to change password please enter current password")
+            password = input("password:")
+            if(password != correctpass):
+                print("sorry password was incorect logging out")
+                loggedin = False
+            else:
+                print ("correct please enter new password")
+                newpassword = input ("new password:")
+                correctpass = newpassword
+        elif (answer == 6):
+            loggedin = False
+            in_bank = False
+            clear()
+    while (in_world == True) :
+        print("whare do you wanna go? \n 1:the bank \n 2:the store")
